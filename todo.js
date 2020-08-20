973ddbf86fcfe91b63b9831ff948dfa4b2a76e4a
diego <webdiegomassari@gmail.com>
2020-08-20 14:44:15 +1000

    first commit

---
+++ todo.js
@@ -0,0 +1,72 @@
+
+
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+
+const generateTemplate = todo =>{
+
+ const html = `<li class="list-group-item flex ">
+ <span>${todo}</span>
+ <i class="far fa-trash-alt delete"></i>
+</li>  `;
+
+
+list.innerHTML += html;
+};
+
+
+//////// ADD
+
+
+addForm.addEventListener ('submit' , e =>{
+
+  e.preventDefault();
+
+  const todo = addForm.add.value.trim(); 
+  if(todo.length){ 
+  
+
+    generateTemplate(todo);
+    addForm.reset();
+    
+    
+  };
+
+});
+
+
+/////DELETE
+
+list.addEventListener('click', e =>{   
+ if(e.target.classList.contains('delete')){
+  e.target.parentElement.remove();
+}  
+
+});
+
+
+
+//// SEARCH
+
+
+const filterTodos = (term) =>{
+ 
+  Array.from(list.children)
+  .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+  .forEach((todo) => todo.classList.add('filter'));
+  
+
+  Array.from(list.children)
+  .filter((todo) =>  todo.textContent.toLowerCase().includes(term))
+  .forEach((todo) => todo.classList.remove('filter')); 
+};
+
+
+search.addEventListener('keyup', () => {
+ 
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+
+});
